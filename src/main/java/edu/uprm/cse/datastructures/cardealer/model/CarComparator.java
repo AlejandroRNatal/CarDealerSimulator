7748dd259205cb1fe6496020079a044282b98fee
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2018-10-01 10:36:10 -0400

    Added classes required by today's lab

---
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarComparator.java
@@ -0,0 +1,24 @@
+package edu.uprm.cse.datastructures.cardealer.model;
+
+import java.util.Comparator;
+
+public class CarComparator implements Comparator{
+
+	
+	public int compare(Car car1, Car car2) {
+		return compare(car1,car2);
+	}
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+//		if(o1 > o2)
+//			return 1;
+//		if(o1 = o2)
+//			return 0;
+//		if(o1 < o2)
+//			return -1;
+		return 0;
+	}
+
+}
