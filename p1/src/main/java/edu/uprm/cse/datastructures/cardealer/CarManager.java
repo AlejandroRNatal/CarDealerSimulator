cffbda477b9846ae28360f57b9061b8a54697f59
Alejandro Rodríguez Natal <rodrigueznatalandy@gmail.com>
2018-10-17 23:22:11 +0000

    Update CarManager.java, removed problematic import statement

--- p1/src/main/java/edu/uprm/cse/datastructures/cardealer/CarManager.java
+++ p1/src/main/java/edu/uprm/cse/datastructures/cardealer/CarManager.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
